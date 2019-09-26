7e0375e17458c15225cb7ebb5a9ef36ef2725bb2
SuperNova911 <suwhan77@naver.com>
2019-09-26 17:10:32 +0900

    Update: Entity, Player 메서드 일부 수정

--- UnityPUBG/Assets/Scripts/Entities/Entity.cs
+++ UnityPUBG/Assets/Scripts/Entities/Entity.cs
@@ -6,14 +6,50 @@ namespace Assets.Scripts.Entities
 {
     public abstract class Entity : MonoBehaviour
     {
-        protected int maximumHealth;
-        protected float currentHealth;
-        protected float movementSpeed;
+        #region 필드
+        [Header("Health")]
+        [SerializeField] protected int maximumHealth;
+        [SerializeField] protected float currentHealth;
+
+        [Header("Movement")]
+        [SerializeField] protected float movementSpeed;
+        [SerializeField] private Vector3 currentVelocity;
+        [SerializeField] private float smoothTime;
+
         protected ItemContainer itemContainer;
 
-        protected void Move(Vector2 direction)
+        private Rigidbody entityRigidbody;
+        #endregion
+
+        #region 유니티 메시지
+        protected virtual void Awake()
         {
-            // move
+            currentHealth = maximumHealth;
+            entityRigidbody = GetComponent<Rigidbody>();
         }
+
+        protected virtual void Start()
+        {
+
+        }
+
+        protected virtual void Update()
+        {
+
+        }
+
+        protected virtual void FixedUpdate()
+        {
+
+        }
+        #endregion
+
+        #region 메서드
+        protected void Move(Vector3 direction)
+        {
+            Vector3 targetVelocity = new Vector3(direction.x * movementSpeed, entityRigidbody.velocity.y, direction.z * movementSpeed);
+            entityRigidbody.velocity = Vector3.SmoothDamp(entityRigidbody.velocity, targetVelocity, ref currentVelocity, smoothTime);
+        } 
+        #endregion
     }
 }
--- UnityPUBG/Assets/Scripts/Entities/Player.cs
+++ UnityPUBG/Assets/Scripts/Entities/Player.cs
@@ -4,14 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Entities
 {
     public class Player : Entity
     {
+        #region 속성
         public Weapon EquipedWeapon { get; private set; }
         public Armor EquipedArmor { get; private set; }
         public Backpack EquipedBackpack { get; private set; }
         public Vehicle RidingVehicle { get; private set; }
+        #endregion
+
+        #region 유니티 메시지
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+        } 
+        #endregion
     }
 }

f3e1c0938e2bdf4d8e49de1313e64863b9ec8540
SuperNova911 <suwhan77@naver.com>
2019-09-26 20:49:03 +0900

    Update: 새로운 InputSystem을 사용하여 플레이어 움직여 보기

--- UnityPUBG/Assets/Scripts/Entities/Player.cs
+++ UnityPUBG/Assets/Scripts/Entities/Player.cs
@@ -1,15 +1,21 @@
 ﻿using Assets.Scripts.Items;
+using Assets.Scripts.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Assets.Scripts.Entities
 {
     public class Player : Entity
     {
+        #region 필드
+        private InputManager inputManager; 
+        #endregion
+
         #region 속성
         public Weapon EquipedWeapon { get; private set; }
         public Armor EquipedArmor { get; private set; }
@@ -18,6 +24,13 @@ namespace Assets.Scripts.Entities
         #endregion
 
         #region 유니티 메시지
+        protected override void Awake()
+        {
+            base.Awake();
+
+            inputManager = new InputManager();
+        }
+
         protected override void Update()
         {
             base.Update();
@@ -26,6 +39,25 @@ namespace Assets.Scripts.Entities
         protected override void FixedUpdate()
         {
             base.FixedUpdate();
+            ControlMovement();
+        }
+
+        private void OnEnable()
+        {
+            inputManager.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputManager.Disable();
+        }
+        #endregion
+
+        #region 메서드
+        private void ControlMovement()
+        {
+            Vector2 direction = inputManager.Player.Movement.ReadValue<Vector2>();
+            Move(direction.x, direction.y);
         } 
         #endregion
     }

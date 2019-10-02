44142ceb52574e944a3e3bfd0ac2c8f92500b49d
G0DCH <2ch1999@naver.com>
2019-10-02 16:49:38 +0900

    Add Character Model
    캐릭터 추가

---
+++ UnityPUBG/Assets/Scripts/Lobby/PTManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PT
+{
+    public class PTManager : Photon.PunBehaviour
+    {
+        #region Photon Messages
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+        {
+            //base.OnPhotonPlayerConnected(newPlayer);
+
+            Debug.Log("OnPhotonPlayerConnected() " + newPlayer.NickName);
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerConnected is MasterClient " + PhotonNetwork.isMasterClient);
+
+                LoadGameRoom();
+            }
+        }
+
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+        {
+            //base.OnPhotonPlayerDisconnected(otherPlayer);
+
+            Debug.Log("OnPhotonPlayerDisconnected() " + otherPlayer.NickName);
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerConnected isMasterClient" + PhotonNetwork.isMasterClient);
+
+                LoadGameRoom();
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            Lobby.LobbyManager.Instance.SendLRoom(PhotonTargets.Others, PhotonNetwork.playerName);
+            PhotonNetwork.LeaveRoom();
+        }
+
+        public void StartGame()
+        {
+            PhotonNetwork.LoadLevel("Game");
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void LoadGameRoom()
+        {
+            if (!PhotonNetwork.isMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+
+            Debug.Log("PhotonNetwork : Loading Level : " + PhotonNetwork.room.PlayerCount);
+            //PhotonNetwork.LoadLevel("Game");
+        }
+
+        #endregion
+    }
+}
---
+++ UnityPUBG/Assets/Scripts/MainMenu/Photon/MenuManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class MenuManager : MonoBehaviour
+    {
+        /// <summary>
+        /// 로그인 화면
+        /// </summary>
+        public GameObject LoginObjects;
+
+        /// <summary>
+        /// Press Any Key라고 적힌 텍스트
+        /// </summary>
+        public Text text;
+
+        /// <summary>
+        /// 글자 점멸 속도
+        /// </summary>
+        float flashSpeed = 0.05f;
+
+        bool isStarted = false;
+
+        private void Start()
+        {
+            text.gameObject.SetActive(true);
+            LoginObjects.SetActive(false);
+            isStarted = false;
+
+            StartCoroutine(FlashText());
+        }
+
+        private void Update()
+        {
+            if (Input.anyKey && !isStarted)
+            {
+                StopCoroutine(FlashText());
+                text.gameObject.SetActive(false);
+                LoginObjects.SetActive(true);
+                isStarted = true;
+            }
+        }
+
+        /// <summary>
+        /// 텍스트 점멸
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator FlashText()
+        {
+            //텍스트의 알파값이 내려가는 중인가?
+            bool isalphadown = true;
+
+            //텍스트의 알파값
+            float alpha = 1f;
+
+            while (true)
+            {
+                if (isalphadown)
+                    alpha -= flashSpeed;
+                else
+                    alpha += flashSpeed;
+
+                if (alpha < 0f)
+                    isalphadown = false;
+                else if (alpha > 1f)
+                    isalphadown = true;
+
+                text.color = new Color(1, 1, 1, alpha);
+
+                yield return new WaitForSeconds(flashSpeed);
+            }
+        }
+    }
+}

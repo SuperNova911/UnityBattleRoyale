c717e305caf94987a3974cac3d8df9c4d7ed2e1a
SuperNova911 <suwhan77@naver.com>
2019-10-12 12:21:31 +0900

    Item을 표현하는 새로운 클래스 작성

---
+++ UnityPUBG/Assets/Scripts/Item/ItemUseable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityPUBG.Scripts.Items
+{
+    public abstract class ItemUseable : Item
+    {
+        public ItemUseable(ItemData data) : base(data)
+        {
+
+        }
+    }
+}

3d29d6cc79b20a6e6cd128d07dfa67404fe091e5
SuperNova911 <suwhan77@naver.com>
2019-10-30 18:25:57 +0900

    CharacterSpawner에서의 Input 방식 변경

--- UnityPUBG/Assets/Scripts/Logic/CharacterSpawn/CharacterSpawner.cs
+++ UnityPUBG/Assets/Scripts/Logic/CharacterSpawn/CharacterSpawner.cs
@@ -1,50 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityPUBG.Scripts.MainMenu;
+using UnityPUBG.Scripts.Utilities;
 
 namespace UnityPUBG.Scripts.CharacterSpawn
 {
     public class CharacterSpawner : MonoBehaviour
     {
-        #region private 변수
+        #region 필드
+        private InputManager inputManager;
+        private CharacterSelecter characterSelecter;
 
-        private MainMenu.CharacterSelecter characterSelecter;
-
-        /// <summary>
-        /// 내 캐릭터를 스폰했는가?
-        /// </summary>
+        // 내 캐릭터를 스폰했는가?
         private bool isSpawned = false;
-
         #endregion
 
-        #region 유니티 콜백
+        #region 유니티 메시지
+        private void Awake()
+        {
+            inputManager = new InputManager();
+        }
 
         private void Start()
         {
-            characterSelecter = FindObjectOfType<MainMenu.CharacterSelecter>().
-                GetComponent<MainMenu.CharacterSelecter>();
+            characterSelecter = FindObjectOfType<CharacterSelecter>().
+                GetComponent<CharacterSelecter>();
         }
 
         private void Update()
         {
-#if (UNITY_ANDROID || UNITY_IPHONE) && !UNITY_EDITOR
-            if (Input.touchCount > 0 && !isSpawned)
+            if (inputManager.CharacterSpawner.Spawn.ReadValue<bool>() && isSpawned == false)
             {
                 characterSelecter.SpawnMyCharacter(transform);
-
                 isSpawned = true;
             }
-#else
-            // 마우스가 눌림
-            if (Input.anyKey && !isSpawned)
-            {
-                characterSelecter.SpawnMyCharacter(transform);
 
-                isSpawned = true;
-            }
-#endif
+//#if (UNITY_ANDROID || UNITY_IPHONE) && !UNITY_EDITOR
+//                        if (Input.touchCount > 0 && !isSpawned)
+//                        {
+//                            characterSelecter.SpawnMyCharacter(transform);
+
+//                            isSpawned = true;
+//                        }
+//#else
+//            if (Input.anyKey && !isSpawned)
+//            {
+//                characterSelecter.SpawnMyCharacter(transform);
+
+//                isSpawned = true;
+//            }
+//#endif
         }
-
         #endregion
     }
 }

780e3b8a0e14296d7a0a8af3ca4aa4d0cf0311d7
G0DCH <2ch1999@naver.com>
2019-11-04 09:19:08 +0900

    Create ItemSyncronizer Script
    Create ItemSyncronizer Script

---
+++ UnityPUBG/Assets/Scripts/Syncronize/ItemSyncronizer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityPUBG.Scripts.Syncronize
+{
+    public class ItemSyncronizer : Photon.MonoBehaviour
+    {
+
+    }
+}

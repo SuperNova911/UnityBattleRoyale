4afccca1203fa2a7d220ebd2101f14f734cba069
SuperNova911 <suwhan77@naver.com>
2019-11-25 20:26:16 +0900

    샌드박스에서 플레이어를 미리 스폰하지 않도록 변경

--- UnityPUBG/Assets/Scripts/ItemContainerDebugger.cs
+++ UnityPUBG/Assets/Scripts/ItemContainerDebugger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
@@ -6,59 +7,63 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using UnityPUBG.Scripts.Entities;
 using UnityPUBG.Scripts.Items;
+using UnityPUBG.Scripts.Logic;
 
 namespace UnityPUBG.Scripts
 {
     public class ItemContainerDebugger : MonoBehaviour
     {
-        [Header("Settings")]
-        [SerializeField] private Player containerOwner;
-
         [Header("UI")]
         [SerializeField] private Text debugText;
+
+        private Player containerOwner;
         private readonly StringBuilder stringBuilder = new StringBuilder();
 
         #region 유니티 메시지
-        private void Start()
+        private void Awake()
         {
-            if (containerOwner != null && containerOwner.ItemContainer != null)
+            EntityManager.Instance.OnMyPlayerSpawn += (object sender, EventArgs e) =>
             {
-                containerOwner.ItemContainer.OnUpdateContainer += ItemContainer_OnUpdateContainer;
-                debugText.text = $"ItemConainer: Count[{containerOwner.ItemContainer.Count}/{containerOwner.ItemContainer.Capacity}]";
-            }
-            else
-            {
-                debugText.text = $"디버깅 할 {nameof(containerOwner.ItemContainer)}를 찾을 수 없습니다";
-            }
+                containerOwner = EntityManager.Instance.MyPlayer;
+                if (containerOwner != null && containerOwner.ItemContainer != null)
+                {
+                    containerOwner.ItemContainer.OnUpdateContainer += ItemContainer_OnUpdateContainer;
+                    debugText.text = $"ItemConainer: Count[{containerOwner.ItemContainer.Count}/{containerOwner.ItemContainer.Capacity}]";
+                }
+                else
+                {
+                    debugText.text = $"디버깅 할 {nameof(containerOwner.ItemContainer)}를 찾을 수 없습니다";
+                }
+            };
         }
 
-        private void Update()
-        {
-            if (Keyboard.current.digit1Key.wasPressedThisFrame)
-            {
-                containerOwner.DropItemsAtSlot(0, 1);
-            }
-            else if (Keyboard.current.digit2Key.wasPressedThisFrame)
-            {
-                containerOwner.DropItemsAtSlot(1, 1);
-            }
-            else if (Keyboard.current.digit3Key.wasPressedThisFrame)
-            {
-                containerOwner.DropItemsAtSlot(2, 1);
-            }
-            else if (Keyboard.current.digit4Key.wasPressedThisFrame)
-            {
-                containerOwner.DropItemsAtSlot(3, 1);
-            }
-            else if (Keyboard.current.digit5Key.wasPressedThisFrame)
-            {
-                containerOwner.DropItemsAtSlot(4, 1);
-            }
-            else if (Keyboard.current.digit6Key.wasPressedThisFrame)
-            {
-                containerOwner.DropItemsAtSlot(5, 1);
-            }
-        }
+        //private void Update()
+        //{
+        //    if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        //    {
+        //        containerOwner.DropItemsAtSlot(0, 1);
+        //    }
+        //    else if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        //    {
+        //        containerOwner.DropItemsAtSlot(1, 1);
+        //    }
+        //    else if (Keyboard.current.digit3Key.wasPressedThisFrame)
+        //    {
+        //        containerOwner.DropItemsAtSlot(2, 1);
+        //    }
+        //    else if (Keyboard.current.digit4Key.wasPressedThisFrame)
+        //    {
+        //        containerOwner.DropItemsAtSlot(3, 1);
+        //    }
+        //    else if (Keyboard.current.digit5Key.wasPressedThisFrame)
+        //    {
+        //        containerOwner.DropItemsAtSlot(4, 1);
+        //    }
+        //    else if (Keyboard.current.digit6Key.wasPressedThisFrame)
+        //    {
+        //        containerOwner.DropItemsAtSlot(5, 1);
+        //    }
+        //}
         #endregion
 
         private void ItemContainer_OnUpdateContainer(object sender, System.EventArgs e)
--- UnityPUBG/Assets/Scripts/Logic/SandboxManager.cs
+++ UnityPUBG/Assets/Scripts/Logic/SandboxManager.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
+using UnityPUBG.Scripts.Entities;
 
 namespace UnityPUBG.Scripts.Logic
 {
     public class SandboxManager : MonoBehaviour
     {
+        public Player testPlayerPrefab;
+        public Vector3 testPlayerSpawnPosition;
+
         private void Awake()
         {
             PhotonNetwork.offlineMode = true;
@@ -18,7 +22,10 @@ namespace UnityPUBG.Scripts.Logic
 
         private void Start()
         {
-
+            if (testPlayerPrefab != null)
+            {
+                PhotonNetwork.Instantiate(testPlayerPrefab.name, testPlayerSpawnPosition, Quaternion.identity, 0);
+            }
         }
     }
 }

7a363ca3287402ee3c94ee46212126b1e40b94e2
SuperNova911 <suwhan77@naver.com>
2019-12-18 15:25:01 +0900

    드랍쉽 모델 추가

---
+++ UnityPUBG/Assets/Scripts/Utility/Rotater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityPUBG.Scripts.Utilities
+{
+    public class Rotater : MonoBehaviour
+    {
+        public Vector3 Speed;
+
+        private void Update()
+        {
+            transform.Rotate(Speed);
+        }
+    }
+}
